378d6189d13b73e210dc6a5a5d4487000c70dc01
Ivan <ivan@ivan-Aspire-4520.(none)>
2014-01-16 16:10:05 -0500

    add sales repost support

---
+++ src/Database/salesmodel.cpp
@@ -0,0 +1,6 @@
+#include "salesmodel.h"
+
+SalesModel::SalesModel(QObject *parent) :
+    QObject(parent)
+{
+}
---
+++ src/Database/salesmodel.h
@@ -0,0 +1,18 @@
+#ifndef SALESMODEL_H
+#define SALESMODEL_H
+
+#include <QObject>
+
+class SalesModel : public QObject
+{
+    Q_OBJECT
+public:
+    explicit SalesModel(QObject *parent = 0);
+
+signals:
+
+public slots:
+
+};
+
+#endif // SALESMODEL_H
